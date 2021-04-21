f42e8781eab4689951e536b8f223f2c0e4a1ff6a
Elia Cools <eliacools@hotmail.com>
2021-04-21 17:03:29 +0200

    commiting all files

---
+++ client/client.js
@@ -0,0 +1,215 @@
+let socket = io.connect();
+
+
+class user {
+    constructor(avatar, username) {
+
+        this.avatar = avatar;
+        this.username = username
+    }
+
+    setAvatar(avatar) {
+        this.avatar = avatar
+    }
+
+    getAvatar(avatar) {
+        return this.avatar
+    }
+
+    getUsername() {
+        return this.username
+    }
+    setId(id){
+        this.id = id;
+    }
+}
+
+document.querySelectorAll(".avatar").forEach(avatar =>
+
+    avatar.addEventListener("click", function () {
+        let inputvalue = avatar.getAttribute("src")
+
+        document.getElementById("avatar-input").setAttribute('value', inputvalue);
+
+        var input = document.getElementById('avatar-input').getAttribute("value")
+
+        if (input === "") {
+            document.getElementById('chooseAvatar').setAttribute("disabled", "")
+
+        } else {
+            createuser.setAvatar(input);
+            let newAvatar = createuser.getAvatar();
+            socket.emit("newAvatar", newAvatar);
+        }
+
+    })
+)
+
+var currentUser;
+var createuser;
+
+while (createuser == null) {
+    let username = prompt("please enter your name")
+    createuser = new user("images/defualt.jpg", username)
+    console.log(createuser.getAvatar())
+
+}
+
+
+if (createuser !== null) {
+    let name = createuser.getUsername();
+    let avatar = createuser.getAvatar();
+    let id = socket.id
+    socket.emit('newUser', {name, avatar, id})
+}
+
+
+
+socket.on('connect', () => {
+    socket.emit('userJoined')
+})
+
+
+
+
+
+// Buttons for sending messages
+document.querySelectorAll('.buttons').forEach(button =>
+
+    button.addEventListener("click", function () {
+        let message = '<img src=' + createuser.getAvatar() + ' class="avatar">' + createuser.getUsername() + " " + document.getElementById("text-input").value
+        let buttonpressed = button.getAttribute("id")
+
+        if (buttonpressed === "to-all") {
+            socket.emit('sendToAll', ({message, room}));
+        }
+        if (buttonpressed === "to-me") {
+            socket.emit('sendToMe', (message))
+        }
+    })
+)
+var room;
+
+document.querySelectorAll('.roomButton').forEach(button => {
+
+    let buttonPressed = button.getAttribute("id")
+    let roomInfo;
+
+    button.addEventListener("click", function () {
+
+        if (buttonPressed === "room1") {
+            room = "room1"
+            socket.emit("create", room);
+
+            roomInfo = "<p> this is room 1 </p>"
+
+        }
+        if (buttonPressed === "room2") {
+            room = "room2"
+            socket.emit("create", room);
+
+            roomInfo = "<p>this is room 2 </p>"
+
+        }
+        socket.emit('message', ({roomInfo, room}))
+        socket.emit('showMessages', room);
+
+    })
+})
+
+
+socket.on('message', (test) => {
+    document.getElementById("text-output-header").innerHTML = test;
+})
+
+
+socket.on('allUsers', (usernames) => {
+    let users = document.getElementById('mainNav')
+    let i = 1;
+    users.innerHTML = "";
+    usernames.forEach(user => {
+        users.innerHTML += '<li id="user' + i++ + '" class="userList" onclick="" value=' + user.id + '>' + '<img src=' + user.avatar + ' class="avatar">' + user.username + '</li>';
+
+    })
+})
+
+var selectedUser;
+socket.on('newEventListener', () => {
+    document.querySelectorAll(".userList").forEach(user => {
+
+        user.addEventListener("click", function () {
+            let id = user.getAttribute('value')
+
+            if (document.getElementById("privateChat").hasAttribute("hidden") === false) {
+                document.getElementById("privateChat").setAttribute("hidden", "");
+            } else {
+                document.getElementById("privateChat").removeAttribute("hidden");
+                console.log(id)
+                // privateMessage(id)
+                document.getElementById("privateTextOutput").innerHTML = "";
+                selectedUser = id;
+                socket.emit("displayPrivate",(id));
+
+            }
+
+        })
+    })
+})
+
+document.getElementById("privateButton").addEventListener("click", function () {
+
+    let message = document.getElementById("private-message").value
+
+    socket.emit("privateMessage", ({message, selectedUser}))
+
+    console.log(message, selectedUser);
+
+})
+
+socket.on("onclickPrivate", (sliced) => {
+    console.log("sender" + sliced[0].sender);
+    console.log("selecteduser" + selectedUser);
+    console.log("sup")
+    sliced[0].messages.forEach(message => {
+
+
+        if(selectedUser === sliced[0].sender ){
+        document.getElementById("privateTextOutput").innerHTML += '<p>' + message + '</p>';
+        console.log("hiiii");
+        }
+
+    })
+
+})
+
+
+
+
+
+
+
+
+
+socket.on('displayMessage', ({room1, room2}) => {
+    document.getElementById("textContainer").innerHTML = ""
+    if (room === "room1") {
+        room1.forEach(messages =>
+
+            document.getElementById("textContainer").innerHTML += '<p>' + messages.message + '</p>'
+        )
+        console.log(room1);
+    }
+    if (room === "room2") {
+        room2.forEach(messages =>
+            document.getElementById("textContainer").innerHTML += '<p>' + messages.message + '</p>'
+        )
+        console.log(room2);
+    }
+
+
+    // document.getElementById("output-room1").innerHTML = "";
+
+    // document.getElementById("text-input").value = "";
+
+});
+
---
+++ server/server.js
@@ -0,0 +1,152 @@
+const express = require('express')
+const http = require('http')
+
+const app = express();
+const server = http.createServer(app)
+const io = require('socket.io')(server);
+
+const port = 8080;
+
+const clientPath = `${__dirname}/../client`;
+app.use(express.static(clientPath))
+
+
+server.listen(8080, () =>{
+    console.log("server running on "+port);
+});
+
+let counter = 0;
+var usernames = [];
+var room1 = [];
+var room2 = [];
+var privateMessages = [];
+
+var avatar;
+
+io.on('connection', (socket) => {
+
+
+
+    socket.on("message", ({roomInfo,room}) =>{
+        io.to(room).emit('message',(roomInfo))
+    });
+    socket.on("message2", (roomInfo) =>{
+        io.to(room).emit('message',(roomInfo))
+    });
+
+
+
+    socket.on('create', function(room) {
+        if (socket.lastRoom) {
+            socket.leave(socket.lastRoom);
+            socket.lastRoom = null;
+        }
+        socket.join(room);
+        socket.lastRoom = room;
+    });
+
+
+
+    console.log(counter++ +' someone connected');
+
+    console.log(socket.id)
+    let name = socket.id
+    io.emit('userids',(name))
+
+    socket.on('showMessages',(room)=>{
+        io.to(room).emit("displayMessage", ({room1,room2}));
+    } )
+
+    socket.on('sendToAll', ({message,room}) =>{
+        if(room=== "room1"){
+            room1.push({message})
+
+        }
+        if(room === "room2"){
+            room2.push({message})
+
+        }
+        console.log(room)
+       // io.to(room).emit("displayMessage", (message));
+        io.to(room).emit("displayMessage", ({room1,room2}));
+
+
+    });
+
+    socket.on("privateMessage", ({message,selectedUser}) =>
+    {
+        let sender = socket.id;
+        let userexists = privateMessages.findIndex(x => x.user === selectedUser)
+        console.log(userexists)
+             if( userexists !== -1){
+                 privateMessages.forEach(privateMessage => {
+                 privateMessage.messages.push(message) ;
+             })
+             }else{
+                 privateMessages.push({"messages":[message],"user":selectedUser,"sender":sender})
+             }
+            console.log(privateMessages.findIndex(x => x.user === selectedUser))
+        let index = privateMessages.findIndex(x => x.selectedUser === selectedUser)
+
+       // privateMessages.slice(index)
+
+      //  io.to(selectedUser).emit("displayPrivate",(message));
+
+        console.log(privateMessages);
+
+      //  socket.emit("displayPrivate",(message));
+    })
+
+    socket.on("displayPrivate",(id) =>{
+
+        if(privateMessages.findIndex(x => x.sender ===  id) !== -1){
+        let index = privateMessages.findIndex(x => x.sender === id )
+            let sliced = privateMessages.slice(index)
+            let slicedMessages = sliced[0].messages;
+            io.to(sliced[0].user).to(sliced[0].sender).emit("onclickPrivate",(sliced))
+
+            console.log(slicedMessages)
+        }
+    })
+
+
+
+    socket.on('disconnect',()=>{
+        console.log(socket.id + " left")
+       let index = usernames.findIndex(x => x.id === socket.id)
+       usernames.splice(index,1);
+        io.emit('allUsers',(usernames));
+    })
+
+    socket.on('userJoined',()=>{
+        let userId = socket.id
+        socket.emit("currentUser",userId)
+
+    })
+
+    socket.on('newUser',({name, avatar}) =>{
+        let id= socket.id;
+        usernames.push({"id":socket.id, "username":name, "avatar": avatar})
+
+        io.emit('allUsers',(usernames));
+        io.emit('newEventListener',(id));
+
+
+    });
+
+    socket.on('newAvatar',(newAvatar) =>{
+        let index = usernames.findIndex(x => x.id === socket.id)
+        usernames[index].avatar = newAvatar;
+        io.emit('allUsers',(usernames));
+    });
+
+     socket.on('sendToMe', (message) =>{
+         socket.emit("displayMessage", (message));
+     });
+
+});
+
+
+
+
+
